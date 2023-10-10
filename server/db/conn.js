41144d40d4f1c536bcd49c45c7e84f16b5e0ce24
raulgamero <raulgamero03@gmail.com>
2023-10-10 18:55:50 +0200

    monogoose

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,22 +1,10 @@
-const { MongoClient } = require("mongodb");
+const mongoose = require('mongoose')
 
-const uri = process.env.ATLAS_URI;
-const client = new MongoClient(uri);
+const connectDB = (url) => {
+    return mongoose.connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+}
 
-let _db;
-
-module.exports = {
-  connectToServer: function (callback) {
-    client.connect(function (err, db) {
-      if (db) {
-        console.log("Successfully connected to MongoDB.");
-        _db = db.db();  // Update _db with the connected database object
-      }
-      return callback(err);
-    });
-  },
-
-  getDb: function () {
-    return _db;
-  },
-};
+module.exports = connectDB
--- server/server.js
+++ server/server.js
@@ -1,17 +1,31 @@
+require("dotenv").config({ path: "./config.env" });
+
+// extra security packages
+const cors = require("cors");
+
 const express = require("express");
 const app = express();
-const cors = require("cors");
-require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
+
+// connectDB
+const connectDB = require('./db/conn');
+
+// routers
+app.use(require("./routes/record"));
+
 app.use(cors());
 app.use(express.json());
-app.use(require("./routes/record"));
-// get driver connection
-const dbo = require("./db/conn");
-app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
-   });
-  console.log(`Server is running on port: ${port}`);
-});
+
+
+const PORT = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+      await connectDB(process.env.ATLAS_URI)
+      app.listen(PORT, () => console.log(`Server listening on port:${PORT}`))
+  }
+  catch (error) {
+      console.log(error)
+  }
+}
+
+start()
